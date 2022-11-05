8269c7265fe1c0e16b524beef55eef635a1e4de7
Amirreza <aazam.amirreza@gmail.com>
2022-11-05 12:43:56 -0400

    initialized the website

---
+++ AmirWebsite/react/build/precache-manifest.a022a3a0bfa9602ed009a9c816c92a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7b0eaf6cd03e73389c782417d2f9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44edbcb7cde485b8f79d",
+    "url": "/static/css/main.1d186e59.chunk.css"
+  },
+  {
+    "revision": "10e07080d09e25fea3b3",
+    "url": "/static/js/2.cac908ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cac908ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44edbcb7cde485b8f79d",
+    "url": "/static/js/main.38843dcc.chunk.js"
+  },
+  {
+    "revision": "56629a03fd211c912c00",
+    "url": "/static/js/runtime-main.390d4b82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ AmirWebsite/react/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Amir() {
+
+  return (
+    <div className="App">
+
+        <div className="center-column">
+
+       
+          <div className="item-row">
+            <span> Hello world</span>
+          </div>
+
+          <div className="item-row">
+            <span>Sean failed civ100"</span>
+          </div>
+
+          <div className="item-row">
+            <span>Drag react app into sean failsssssssject</span>
+          </div>
+
+          <div className="item-row">
+            <span>I love dogs & cats $ myself</span>
+          </div>
+
+          <div className="item-row">
+            <span>Configure URL path</span>
+          </div>
+
+          <div className="item-row">
+            <span>Configure static files</span>
+          </div>
+
+          <div className="item-row">
+            <span>cd sdfswfwefwe react app and run "npm run build"</span>
+          </div>
+
+        </div>
+     
+    </div>
+  );
+}
+
+export default Amir;
